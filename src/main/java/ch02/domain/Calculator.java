5b151f2ed8fe4576cb968100932f63afbdaafa5b
박승민 <tmddudals369@naver.com>
2023-02-14 03:25:02 +0900

    feat: 음수가 존재할 경우 에러 발생 기능 구현

--- src/main/java/ch02/domain/Calculator.java
+++ src/main/java/ch02/domain/Calculator.java
@@ -3,9 +3,18 @@ package ch02.domain;
 import java.util.Arrays;
 
 public class Calculator {
+
     public int add(String... numbers) {
         return Arrays.stream(numbers)
                 .mapToInt(Integer::parseInt)
+                .filter(Calculator::validNegative)
                 .sum();
+    }
+
+    private static boolean validNegative(int num) {
+        if (num < 0) {
+            throw new RuntimeException();
+        }
+        return true;
     }
 }
