5a3800adb1936795911bf92c55f551bffc5c4fee
박승민 <tmddudals369@naver.com>
2023-02-18 02:21:44 +0900

    feat: 구현한 기능 조립

--- src/main/java/ch02/Main.java
+++ src/main/java/ch02/Main.java
@@ -1,4 +1,19 @@
 package ch02;
 
+import ch02.domain.Calculator;
+import ch02.domain.NumberExtractor;
+import ch02.validation.Validator;
+import ch02.view.InputView;
+import ch02.view.OutputView;
+
 public class Main {
+    public static void main(String[] args) {
+        String readNumber = InputView.readNumber();
+        NumberExtractor numberExtractor = new NumberExtractor();
+        String[] numbers = numberExtractor.extractNumber(readNumber);
+        Validator.validNumber(numbers);
+        Validator.validateNegative(numbers);
+        Calculator calculator = new Calculator(numbers);
+        OutputView.printResult(calculator.sum());
+    }
 }
