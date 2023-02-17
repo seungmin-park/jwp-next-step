c001bd934ebc8f45b1208cc38dfca71c3e5b2138
박승민 <tmddudals369@naver.com>
2023-02-18 02:18:33 +0900

    feat: 커스텀 구분자 기능 구현

--- src/main/java/ch02/domain/NumberExtractor.java
+++ src/main/java/ch02/domain/NumberExtractor.java
@@ -1,5 +1,8 @@
 package ch02.domain;
 
+import ch02.validation.Validator;
+import java.util.StringTokenizer;
+
 public class NumberExtractor {
 
     private static final String REST_SEPARATOR = ",";
@@ -7,12 +10,25 @@ public class NumberExtractor {
     private static final String BASIC_SEPARATOR = ",|:";
     private static final String CUSTOM_SEPARATOR_PREFIX = "//";
     private static final String CUSTOM_SEPARATOR_SUFFIX = "\\n";
-
     public String[] extractNumber(String inputNumber) {
+        if (Validator.validateEmptyInput(inputNumber)) {
+            return new String[]{"0"};
+        }
+
+        if (containCustomSeparator(inputNumber)) {
+            String customSeparator = extractCustomSeparator(inputNumber);
+            String removeSeparatorNumber = inputNumber.substring(5);
+            if (containBasicSeparator(inputNumber)) {
+                return removeSeparatorNumber.split(customSeparator + "|" + BASIC_SEPARATOR);
+            }
+            return removeSeparatorNumber.split(customSeparator);
+        }
+
         if (containBasicSeparator(inputNumber)) {
             return inputNumber.split(BASIC_SEPARATOR);
         }
-        throw new IllegalArgumentException();
+
+        return new String[]{inputNumber};
     }
 
     private boolean containBasicSeparator(String inputNumber) {
@@ -24,7 +40,7 @@ public class NumberExtractor {
     }
 
     public String extractCustomSeparator(String inputNumber) {
-        return inputNumber.split(String.join(CUSTOM_SEPARATOR_PREFIX, CUSTOM_SEPARATOR_PREFIX))[1];
+        return new StringTokenizer(inputNumber, "//\\n").nextToken();
     }
 
     private boolean containCustomSeparator(String inputNumber) {
