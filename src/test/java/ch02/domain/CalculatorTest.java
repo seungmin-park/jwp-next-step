9357fd0d483279421bc39e6eb5cec178290c918a
박승민 <tmddudals369@naver.com>
2023-02-18 02:16:52 +0900

    fix: 더하기 메소드 분리로 인한 테스트 코드 변경

--- src/test/java/ch02/domain/CalculatorTest.java
+++ src/test/java/ch02/domain/CalculatorTest.java
@@ -1,5 +1,6 @@
 package ch02.domain;
 
+import ch02.validation.Validator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -7,13 +8,12 @@ class CalculatorTest {
 
     @Test
     void add_test() {
-        Calculator calculator = new Calculator();
-        int result = calculator.add("1", "2", "3");
+        Calculator calculator = new Calculator("1", "2", "3");
+        int result = calculator.sum();
         Assertions.assertEquals(result, 6);
     }
     @Test
     void add_throw_exception() {
-        Calculator calculator = new Calculator();
-        Assertions.assertThrowsExactly(RuntimeException.class, () -> calculator.add("-1", "2", "3"));
+        Assertions.assertThrowsExactly(RuntimeException.class, () -> Validator.validateNegative("-1", "2", "3"));
     }
 }
